b3379f64fbe478a700d3b3ff427e907cfcd85fc3
royliu007 <haijunliu07@foxmail.com>
2019-12-26 23:36:48 +0800

    add count down new year demo

---
+++ NewYearCountDown/index.js
@@ -0,0 +1,26 @@
+//获取2020年1月1日的时间戳
+let countDate = new Date('Jan 1, 2020 00:00:00').getTime();
+
+function newYear() {
+  let now = new Date().getTime()
+  let gap = countDate - now
+
+  let second = 1000
+  let minute = second * 60
+  let hour = minute * 60
+  let day = hour * 24
+
+  let d = Math.floor(gap / (day))
+  let h = Math.floor((gap % (day)) / (hour))
+  let m = Math.floor((gap % (hour)) / (minute))
+  let s = Math.floor((gap % (minute)) / (second))
+
+  document.getElementById('day').innerText = d
+  document.getElementById('hour').innerText = h
+  document.getElementById('minute').innerText = m
+  document.getElementById('second').innerText = s
+}
+
+setInterval(() => {
+  newYear()
+}, 1000);
